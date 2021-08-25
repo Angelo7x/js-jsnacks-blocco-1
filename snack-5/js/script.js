d8558eb8678ad0a6823b8559be24e7d065ef31cc
Angelo7x <abennici96@gmail.com>
2021-08-25 16:55:35 +0200

    Esercizio finito

--- snack-5/js/script.js
+++ snack-5/js/script.js
@@ -2,10 +2,13 @@
 var listaNumeri = [];
 //creo ciclo con la possibilità di inserire numeri
 for (var i = 0 ; i < 6 ; i++) {
-    var numeroUtente = prompt("Inserisci un numero") ;
+    var numeroUtente = parseInt(prompt("Inserisci un numero")) ;
+    if ( numeroUtente %2 != 0) {
+        var numeroAggiunto = listaNumeri.push(numeroUtente)
+    }
 }
+console.log(listaNumeri)
 
-if (numeroUtente)
 
 
 //verifico che i numeri siano pari o dispari
