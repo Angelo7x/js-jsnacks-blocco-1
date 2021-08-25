f39b347695107d897c0d0fde7701bcfbfcc0472b
Angelo7x <abennici96@gmail.com>
2021-08-25 16:38:44 +0200

    struttura iniziale

---
+++ snack-5/js/script.js
@@ -0,0 +1,14 @@
+//creo array vuota
+var listaNumeri = [];
+//creo ciclo con la possibilità di inserire numeri
+for (var i = 0 ; i < 6 ; i++) {
+    var numeroUtente = prompt("Inserisci un numero") ;
+}
+
+if (numeroUtente)
+
+
+//verifico che i numeri siano pari o dispari
+//i dispari vanno inseriti
+
+
